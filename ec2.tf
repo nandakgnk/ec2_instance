c14f5bdbcf6bedf7895d5e73828f547478ee191b
Nanda Kishore <nandak.gnk@gmail.com>
2023-07-05 17:16:05 +0000

    Add new file

---
+++ ec2.tf
@@ -0,0 +1,29 @@
+module "ec2_east" {
+  source            = "./modules/ec2"
+  ec2_count         = var.east_ec2_count
+  aws_image         = var.east_aws_image
+  aws_instance_type = var.east_aws_instance_type
+  ec2_keypair       = var.east_keypair
+  subnetid          = data.aws_subnet.east_subnetid.id
+  securitygid       = ["${data.aws_security_group.east_sgroupid.id}"]
+  user_data         = file("east_user_data.sh")
+  extra_tags = {
+    Name        = "server-awsami"
+    Environment = "Devlopment"
+  }
+}
+
+module "ec2_west1" {
+  source            = "./modules/ec2"
+  ec2_count         = var.west_ec2_count
+  aws_image         = var.west_aws_image
+  aws_instance_type = var.west_aws_instance_type
+  ec2_keypair       = var.west_keypair
+  subnetid          = data.aws_subnet.east_subnetid.id
+  securitygid       = ["${data.aws_security_group.east_sgroupid.id}"]
+  user_data         = file("west_user_data.sh")
+  extra_tags = {
+    Name        = "Server1-ubuntu",
+    Environment = "Developmemt"
+  }
+}
