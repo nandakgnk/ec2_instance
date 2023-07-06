f3c5c7b639d982fb430f8a32cbb89f0dc1b60c96
Nanda Kishore <nandak.gnk@gmail.com>
2023-07-06 14:42:52 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,8 @@ module "ec2_east" {
   aws_image         = var.east_aws_image
   aws_instance_type = var.east_aws_instance_type
   ec2_keypair       = var.east_keypair
-  subnetid          = data.aws_subnet.east_subnetid.id
+  subnetid          = tolist(data.aws_subnet.east_subnetid.ids)[count.index %
+length(data.aws_subnet_ids.east_subnetid.ids)   ]
   securitygid       = ["${data.aws_security_group.east_sgroupid.id}"]
   user_data         = file("east_user_data.sh")
   extra_tags = {
@@ -20,7 +21,9 @@ module "ec2_west1" {
   aws_image         = var.west_aws_image
   aws_instance_type = var.west_aws_instance_type
   ec2_keypair       = var.west_keypair
-  subnetid          = data.aws_subnet.east_subnetid.id
+ # subnetid          = data.aws_subnet.east_subnetid.id
+  subnetid          = tolist(data.aws_subnet.east_subnetid.ids)[count.index %
+length(data.aws_subnet_ids.east_subnetid.ids)   ] 
   securitygid       = ["${data.aws_security_group.east_sgroupid.id}"]
   user_data         = file("west_user_data.sh")
   extra_tags = {
