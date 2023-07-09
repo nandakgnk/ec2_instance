31361341e36c33760a2826a8f336f7d7e94aba50
Nanda Kishore <nandak.gnk@gmail.com>
2023-07-09 04:55:25 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,10 +4,10 @@ terraform {
 #    key            = "terraform/statefile"
 #    region         = "us-east-1"
 #    dynamodb_table = "terraform_state"
-   bucket         = var.east_bname"myownuse-terraform-state-backend"
-   key            = var.east_key" terraform/statefile"
+   bucket         = var.east_bname
+   key            = var.east_key
    region         = var.aws_region
-   dynamodb_table = var.east_dyn_db "terraform_state"
+   dynamodb_table = var.east_dyn_db
 
   }
 }  
